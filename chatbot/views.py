38d57030adcc30280909d8ca346136a1dcd82819
Kuldeep Bhatia <kuldeep_bhatia03@icloud.com>
2024-03-20 20:52:02 +1100

    Ai chatbot

---
+++ chatbot/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+open_api_key = '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'
+openai.api_key = open_api_key
+
+def ask_openai(message):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": "You are an helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+
+
+    )
+
+    answer = response.choices[0].message.content.strip()
+    return answer
+
+
+
+def chatbot(request):
+    if request.method == 'POST':
+        message= request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message,'response': response })
+    return render(request,  'chatbot.html')
+
+
+
+
+
+
